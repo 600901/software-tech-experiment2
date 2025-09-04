cc914295ecfc5192f7d377b83011b8decb6da434
600901 <600901@stud.hvl.no>
2025-09-04 16:33:11 +0200

    first commit

---
+++ src/main/java/no/xiv/software_tech_experiment2/SoftwareTechExperiment2Application.java
@@ -0,0 +1,13 @@
+package no.xiv.software_tech_experiment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareTechExperiment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareTechExperiment2Application.class, args);
+	}
+
+}
---
+++ src/test/java/no/xiv/software_tech_experiment2/SoftwareTechExperiment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.xiv.software_tech_experiment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareTechExperiment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
